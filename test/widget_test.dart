c98c62f3415f26200cdf9603e7b08c8c3d9fe9db
tsparks9038 <torin.sparks@gmail.com>
2025-04-08 15:32:36 -0400

    enabled internet on release version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:khinsider_android/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SearchApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
